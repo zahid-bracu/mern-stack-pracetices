fd9a069d04e6fcfa87f1667f7abf3f0e49ed8a8b
Md. Zahidur Rahman <67168046+zahid-bracu@users.noreply.github.com>
2020-08-14 15:17:49 +0600

    new

--- JavaScript/Module26/file.js
+++ JavaScript/Module26/file.js
@@ -4,12 +4,12 @@ var person={
     id: 420,
     salary: 3000,
   
-    // function inside object
+    // --------------function inside object----------------
     getName : function(){
       return "My Name is "+ this.fname +" "+this.lname;
     },
   
-    //function inside object with return value
+    //---------------function inside object with return value------------
     withdraw : function(amount){
       this.salary=this.salary-amount;
       return this.salary;
@@ -38,16 +38,51 @@ var person={
     salary: 8000
   }
   
-  //binding
+  //----------------------binding------------------------
   var foulSalary=person.withdraw.bind(foulPerson);
   var value=foulSalary(550);
   console.log(value);
   
-  //calling
+  //--------------------------call--------------------------------------
   person.withdraw.call(heroPerson, 120);
   console.log(heroPerson.salary);
   
-  //apply
+  //-------------------------apply---------------------------------
   person.withdraw.apply(dashingPerson, [2873]);
   console.log(dashingPerson.salary);
+  
+
+  //------------------------------ Make a local variable to global-----------------------------
+  function add(num1,num2){
+    result=num1+num2;
+    return result;
+  }
+  
+  //console.log(result);
+  var val=add(10,20);
+  console.log(result);
+
+
+  // ----------------------- Class and Function with this & new-----------------------------
+
+  class person{
+    constructor(name,id){
+      //this
+      this.name=name,
+      this.id=id
+    }
+  }
+  //new
+  const object=new person("Kislu",420);
+  console.log(object);
+  
+  
+  
+  //function with this & new
+  function student(name,id){
+    this.name=name,
+    this.id=id
+  }
+  const info=new student("Hublo",15101122);
+  console.log(info);
   
