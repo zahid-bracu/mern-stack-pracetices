27ace6c269990af160d8aeb4a6d9c440806cbe2b
Md. Zahidur Rahman <67168046+zahid-bracu@users.noreply.github.com>
2020-09-05 03:11:05 +0600

    update

--- Projects/Course/mycourse/src/Cart.js
+++ Projects/Course/mycourse/src/Cart.js
@@ -7,20 +7,36 @@ import {
 
   
 function Cart(props){
+
     
+
     var total=0;
     for(let i=0;i<props.cart.length;i++){
         total+=props.cart[i].price;
     }
+
+    function checkOut(){
+        if(total>0){
+            document.getElementById('checking').style.display="none";
+            document.getElementById('msg').style.display="block";
+        }
+        
+    }
+
     return(
-        <Card className="my-4" style={{position:'sticky',top:'0',width: 'auto',border:'none' }}>
-            <Card.Body>
+        <Card  className="my-4" style={{position:'sticky',top:'0',width: 'auto',border:'none' }}>
+            <Card.Body id="checking">
                 <Card.Title>Cart</Card.Title>
                 <p>Total Courses: {props.cart.length}</p>
                 <p>Total Price: {total}</p>
-                <Button variant="danger" size="sm">Check Out</Button>
+                <Button onClick={(event) => {checkOut(event)}} variant="danger" size="sm">Check Out</Button>
+            </Card.Body>
+
+            <Card.Body id="msg" style={{display:"none"}}>
+                <h3 className="text-center">Checked Out Successfully</h3>
             </Card.Body>
         </Card>
+
     )
 }
 
--- Projects/Course/mycourse/src/Display.js
+++ Projects/Course/mycourse/src/Display.js
@@ -9,37 +9,20 @@ export default function Display(props) {
   const {name,id,tag,img}=props.Datas;
   const [price,setPrice]=useState();
 
-
-    
    
 
-  // function setPrices(value){
-  //   if(price==null){
-  //     var newValue=value+"$";
-  //     setPrice(newValue);
-  //     var buttons=document.querySelectorAll("#button");
-  //     for(var i=0;i<buttons.length;i++){
-  //       var item=buttons[i];
-  //       item.addEventListener("click",function(e){
-  //         e.target.innerText="Hide Price";
-  //       });
-  //     }
-  //   }else{
-  //     var temp=null;
-  //     setPrice(temp);
-  //     var buttons=document.querySelectorAll("#button");
-  //     for(var i=0;i<buttons.length;i++){
-  //       var item=buttons[i];
-  //       item.addEventListener("click",function(e){
-  //         e.target.innerText="Show Price";
-  //       });
-  //     }
-  //   }
-  // }
-
-  document.getElementById("button").addEventListener('click',function(event){
-    console.log(event.target.innerText);   
-  });
+  function setPrices(value,event){
+    console.log();
+    if(price==null){
+      var newValue=value+"$";
+      setPrice(newValue);
+      event.target.innerText="Hide Price";
+    }else{
+      var temp=null;
+      setPrice(temp);
+      event.target.innerText="Show Price";
+    }
+  }
 
   return (
       <Card className=" col-lg-6 col-md-6 col-sm-12 col-12 m-4" style={{maxWidth: '18rem' }}>
@@ -53,8 +36,8 @@ export default function Display(props) {
                 <h6>{price}</h6>
                 </Card.Text>
               
-              <Button id="button"  className="my-2" style={{display:"block"}} variant="info" size="sm">Show Price</Button>
-              <Button   onClick={()=> {props.addCart(props.Datas)}} variant="secondary" size="sm">Add to Cart</Button>
+              <Button   onClick={(event)=>setPrices(props.Datas.price,event)} className="my-2" style={{display:"block"}} variant="info" size="sm">Show Price</Button>
+              <Button   onClick={()=> props.addCart(props.Datas)} variant="secondary" size="sm">Add to Cart</Button>
           </Card.Body>
       </Card>
   );
