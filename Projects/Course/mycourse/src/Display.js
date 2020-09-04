e11eef7979914b032ea473e05fd5dda066b4e247
Md. Zahidur Rahman <67168046+zahid-bracu@users.noreply.github.com>
2020-09-05 02:38:27 +0600

    Update Display.js

--- Projects/Course/mycourse/src/Display.js
+++ Projects/Course/mycourse/src/Display.js
@@ -9,20 +9,38 @@ export default function Display(props) {
   const {name,id,tag,img}=props.Datas;
   const [price,setPrice]=useState();
 
+
+    
    
 
-  function setPrices(value){
-    if(price==null){
-      var newValue=value+"$";
-      setPrice(newValue);
-    }else{
-      var temp=null;
-      setPrice(temp);
-    }
-    
-     
-  }
- 
+  // function setPrices(value){
+  //   if(price==null){
+  //     var newValue=value+"$";
+  //     setPrice(newValue);
+  //     var buttons=document.querySelectorAll("#button");
+  //     for(var i=0;i<buttons.length;i++){
+  //       var item=buttons[i];
+  //       item.addEventListener("click",function(e){
+  //         e.target.innerText="Hide Price";
+  //       });
+  //     }
+  //   }else{
+  //     var temp=null;
+  //     setPrice(temp);
+  //     var buttons=document.querySelectorAll("#button");
+  //     for(var i=0;i<buttons.length;i++){
+  //       var item=buttons[i];
+  //       item.addEventListener("click",function(e){
+  //         e.target.innerText="Show Price";
+  //       });
+  //     }
+  //   }
+  // }
+
+  document.getElementById("button").addEventListener('click',function(event){
+    console.log(event.target.innerText);   
+  });
+
   return (
       <Card className=" col-lg-6 col-md-6 col-sm-12 col-12 m-4" style={{maxWidth: '18rem' }}>
         <Card.Img variant="top" src={img} />
@@ -35,8 +53,8 @@ export default function Display(props) {
                 <h6>{price}</h6>
                 </Card.Text>
               
-              <Button   onClick={()=>setPrices(props.Datas.price)} className="my-2" style={{display:"block"}} variant="info" size="sm">Show Price</Button>
-              <Button   onClick={()=> props.addCart(props.Datas)} variant="secondary" size="sm">Add to Cart</Button>
+              <Button id="button"  className="my-2" style={{display:"block"}} variant="info" size="sm">Show Price</Button>
+              <Button   onClick={()=> {props.addCart(props.Datas)}} variant="secondary" size="sm">Add to Cart</Button>
           </Card.Body>
       </Card>
   );
