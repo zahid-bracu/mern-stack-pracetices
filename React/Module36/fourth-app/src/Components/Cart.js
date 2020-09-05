83b51665125efaa676f977c9173507c2f0933545
Md. Zahidur Rahman <67168046+zahid-bracu@users.noreply.github.com>
2020-09-05 15:57:41 +0600

    Update Cart.js

--- React/Module36/fourth-app/src/Components/Cart.js
+++ React/Module36/fourth-app/src/Components/Cart.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
 
+
 const Cart = (props) => {
 
     console.log(props);
     const {title,author,description}=props.article;
     return (
-        <div>
+        <React.Fragment>
             <Card className="m-2" style={{ maxWidth: '300px',border:"none" }}>
                 <Card.Body>
                     <Card.Title>{title}</Card.Title>
-                        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-                            <Card.Text style={{overflow:"hidden"}}>
+                        <Card.Subtitle className="mb-2 text-muted">{author}</Card.Subtitle>
+                            <Card.Text style={{minHeight:"100px",overflow:"hidden"}}>
                                 {description}
                             </Card.Text>
-                        <Card.Link href="#">Card Link</Card.Link>
-                    <Card.Link href="#">Another Link</Card.Link>
+                        <Card.Link href="#">Read More</Card.Link>
                 </Card.Body>
             </Card>
-        </div>
+        </React.Fragment>
     );
 };
 
