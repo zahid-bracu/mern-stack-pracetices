5e83aea31d05caac683df8c65594a68a122f62c6
Md. Zahidur Rahman <67168046+zahid-bracu@users.noreply.github.com>
2020-11-06 21:33:36 +0600

    update

--- Mongo_Node/module48-mongo-node-crud/index.js
+++ Mongo_Node/module48-mongo-node-crud/index.js
@@ -3,14 +3,16 @@ const app=express();
 const MongoClient = require('mongodb').MongoClient;
 
 
-const uri = "mongodb+srv://demoUser:9augustbd@cluster0.rwjuz.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const uri = "mongodb+srv://demoUser:9augustbd@cluster0.rwjuz.mongodb.net/newDB?retryWrites=true&w=majority";
 
 
-const client = new MongoClient(uri, { useNewUrlParser: true });
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+  const collection = client.db("newDB").collection("testDB");
+  collection.insertOne({name:"Zahid",id:420})
+  .then(res=>{
+      console.log("Added");
+  })
 });
 
 
