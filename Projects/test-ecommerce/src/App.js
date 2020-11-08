e7bcc38e9e4076330ecb4c9ae244d04bb9346da1
Md. Zahidur Rahman <67168046+zahid-bracu@users.noreply.github.com>
2020-11-08 19:55:24 +0600

    up

---
+++ Projects/test-ecommerce/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function App() {
+  const imgUrl = "https://i.ibb.co/8Kt454R/mis.jpg";
+  return (
+    <div className="wrapper">
+      <div className="card">
+        <img src={imgUrl} style={{ width: "192px", height: "210px" }} />
+        <div className="titles">
+          <h6>Kamiz</h6>
+        </div>
+        <div className="footers d-flex justify-content-between">
+          <h6>BDT. 7,850</h6>
+          <span class="badge badge-warning">15%</span>
+        </div>
+        
+      </div>
+    <button className="btn btn-warning btn-cust">Add to cart</button>
+    </div>
+  );
+}
