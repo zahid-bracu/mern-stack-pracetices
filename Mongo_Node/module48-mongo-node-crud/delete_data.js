50c9fc845953e7da145418b54a3992eab22374f1
Md. Zahidur Rahman <67168046+zahid-bracu@users.noreply.github.com>
2020-11-08 16:41:52 +0600

    update

--- Mongo_Node/module48-mongo-node-crud/delete_data.js
+++ Mongo_Node/module48-mongo-node-crud/delete_data.js
@@ -2,20 +2,24 @@ const express=require('express'); //always use
 const app=express(); // always use
 
 
-var bodyParser = require('body-parser') //use for post data
+
+var bodyParser = require('body-parser'); //use for post data
 app.use(bodyParser.urlencoded({ extended: false })) // use for post data
 app.use(bodyParser.json()) // use for post data
 
+const { ObjectID } = require('mongodb'); // use to compare id in the db + connect mongo
 
 const MongoClient = require('mongodb').MongoClient; //use for connect mongo
 const uri = "mongodb+srv://demoUser:9augustbd@cluster0.rwjuz.mongodb.net/newDB?retryWrites=true&w=majority"; // use for connect mongo
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true }); // use for connect mongo
 
 
-// below code for post and get data
+// below code for  get data
 client.connect(err => {
   const collection = client.db("newDB").collection("testDB");
 
+
+//   get data as to array
   app.get('/products',(req,res)=>{
     collection.find({})
     .toArray((err,document)=>{
@@ -25,7 +29,7 @@ client.connect(err => {
     })
     })
   
-//  moving to add product page
+//  post data as 
   app.post('/addProduct',(req,res)=>{
       const product=req.body; // getting the form input
       console.log(product);
@@ -38,6 +42,16 @@ client.connect(err => {
 
   })
 
+
+//   delete data
+  app.delete('/delete/:id', (req,res)=>{
+      console.log(req.params.id);
+      collection.deleteOne({_id:ObjectID(req.params.id)})
+      .then(result=>{
+          console.log(result);
+      })
+  })
+
   
 });
 
@@ -47,4 +61,4 @@ app.get('/',(req,res)=>{
     res.sendFile(__dirname+'/index.html');
 })
 
-app.listen(3001);
+app.listen(3002);
