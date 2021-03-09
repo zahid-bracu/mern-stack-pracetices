51d0b9a963db7d02359d8a7190333f7129ed37b2
Md. Zahidur Rahman <67168046+zahid-bracu@users.noreply.github.com>
2021-03-10 03:11:12 +0600

    upredux

--- Redux/Redux/redux-app/src/App.js
+++ Redux/Redux/redux-app/src/App.js
@@ -2,14 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import ReducerCount from './components/ReducerCount';
 import PatientManagment from './components/patients/PatientManagment';
- 
+import RandomPatient from './components/patients/RandomPatient';
 
 function App() {
   return (
     <div className="App">
       <ReducerCount/>
       <PatientManagment/>
-      
+      <RandomPatient/>
     </div>
   );
 }
--- Redux/Redux/redux-app/src/components/patients/patientReducer.js
+++ Redux/Redux/redux-app/src/components/patients/patientReducer.js
@@ -6,9 +6,16 @@ export const patientState={
 export const PatientReducer=(state,action)=>{
     switch(action.type){
         case 'ADD_PATIENT':
-            return state;
+            const newPatient={
+                id:action.id,
+                name:action.name 
+            }
+            const allPatients=[...state.patients,newPatient];
+            return {patients: allPatients}
         case 'REMOVE_PATIENT':
-            return state;
+            const remain=state.patients.filter(key=> key.id !==action.id);
+            const newState={patients : remain};
+            return newState;
         default:
             return state;
     }
