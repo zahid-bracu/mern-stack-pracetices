a4d73c89af3384908c8def0bc599062816892131
Muhammad Zahidur Rahman <zahid.mym@gmail.com>
2021-04-04 23:39:02 +0600

    up

--- Projects/yelp-pern-stack/server/db.sql
+++ Projects/yelp-pern-stack/server/db.sql
@@ -1,5 +1,53 @@
--- for help : \?
--- list of database : \l
--- for help : \help
+-- tutorial site :  
+https://www.postgresqltutorial.com/ 
+ 
 
---create databse : CREATE DATABASE yelp;
+
+-- for help : 
+\?
+-- list of database : 
+\l
+-- for help : 
+\help
+
+--create databse : 
+CREATE DATABASE yelp;
+
+--create table : 
+CREATE TABLE products (
+	id INT NOT NULL,
+	name VARCHAR(500),
+	price INT NOT NULL,
+	on_sale boolean
+  );
+
+
+--add new column : 
+ALTER table products add column featured boolean;
+
+--delete column :
+ ALTER table products delete column featured boolean;
+
+--drop table : 
+Drop table table_name;
+
+--drop db : 
+DROP database db;
+
+
+--create table for yelp projects
+CREATE TABLE restaurants(
+    id INT,
+    name VARCHAR(50),
+    location VARCHAR(50),
+    price_range INT
+);
+
+
+--insert data to table
+INSERT INTO restaurants(id, name, location, price_range)
+VALUES (123, 'mcdonalds', 'new york', 3);
+
+
+--show data from table
+SELECT * FROM restaurants;
