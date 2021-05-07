5cc31514ba5135636f020e320e8b765b791eae59
zahid-bracu <zahid.mym@gmail.com>
2021-05-08 02:54:33 +0600

    ok

--- redux-tut/src/state-managment/reducer/userReducer.js
+++ redux-tut/src/state-managment/reducer/userReducer.js
@@ -6,19 +6,27 @@ const initialState={
     error:''
 }
 
-// export default function(state=initialState,action) {
-//     switch(action.type){
-//         case increment_value:
-//             return{
-//                 ...state,
-//                 count:state.count+action.payload
-//             }
-//         case decrement_value:
-//             return{
-//                 ...state,
-//                 count:state.count-action.payload
-//             }
-//         default:
-//             return state;
-//     }
-// }
+export default function(state=initialState,action) {
+    switch(action.type){
+        case users_request:
+            return{
+                ...state,
+                loading:true
+            }
+        case get_users_success:
+            return{
+                ...state,
+                loading:false,
+                user:action.payload
+            }
+        case get_users_failed:
+            return{
+                ...state,
+                loading:false,
+                user:[],
+                error:action.payload
+            }
+        default:
+            return state;
+    }
+}
