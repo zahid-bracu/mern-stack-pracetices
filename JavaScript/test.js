eb9a977e08eadbfddde89ced02bbfb35d9e10992
zahid-bracu <zahid.mym@gmail.com>
2021-06-03 15:28:16 +0600

    ok

---
+++ JavaScript/test.js
@@ -0,0 +1,8 @@
+var array=[1,3,5,7,11];
+var key=6;
+var temp=0;
+function test(value){
+ for(var i=0;i<array.length;i++){
+
+ }
+}
--- Projects/test-ecommerce/src/App.js
+++ Projects/test-ecommerce/src/App.js
@@ -1,43 +1,11 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import React, { useEffect, useState } from "react";
-import "./App.css";
-import Cart from './Cart';
-import Display from "./Display";
-import mockAxiosCall from './mockAxiosCall';
+import React from 'react';
 
-export default function App() {
-  const [cart,setCart]=useState([]);
-  const [data,setData]=useState([]);
-  useEffect(() => {
-    console.log(mockAxiosCall().shareClassArray);
-    var arrayObObject=mockAxiosCall().shareClassArray;
-    setData(arrayObObject);
-  }, 0)
+const App = () => {
+ return (
+  <div>
+   <h1>Boiler Plate</h1>
+  </div>
+ );
+};
 
-
-  function addToCart(name){
-    console.log(name);
-    
-    const temp=[...cart,name];
-    setCart(temp);
-  }
-
-  console.log(cart);
-  console.log(data);
-  const imgUrl = "https://i.ibb.co/8Kt454R/mis.jpg";
-  return (
-    <>
-         <div>
-           {
-             data.map(key=> <Display addToCart={addToCart} pd={key}></Display>)
-           }
-         </div>
-
-         <h1>Cart L : {cart.length}</h1>
-         {
-
-            cart.map(key => <Cart pd={key}></Cart>)
-         }
-    </>
-  );
-}
+export default App;
